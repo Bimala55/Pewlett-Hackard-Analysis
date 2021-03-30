66e53f71cc4646fd6125e043596754bb12f971e9
Bimala Basnet <bbimala85@gmail.com>
2021-03-30 09:32:33 -0400

    Added Database schema

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,4 +1,4 @@
--- Creating tables for PH-EmployeeDB
+--- Creating tables for PH-EmployeeDB
 CREATE TABLE Departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -37,13 +37,6 @@ CREATE TABLE  Dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	PRIMARY KEY(emp_no, dept_no)
-);
-CREATE TABLE Titles(
-	emp_no VARCHAR NOT NULL,
-	title INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager(
@@ -87,13 +80,94 @@ CREATE TABLE  Dept_emp (
 FOREIGN KEY (dept_no) REFERENCES Department (dept_no),	
 	PRIMARY KEY(emp_no, dept_no)
 );
-CREATE TABLE Titles(
-	emp_no VARCHAR NOT NULL,
-	title INT NOT NULL,
+
+CREATE TABLE Employees (
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT 	NULL,
+	PRIMARY KEY(emp_no)
+	);
+CREATE TABLE Salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES Salaries(emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY(emp_no)
+	);
+CREATE TABLE Dept_manager(
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, dept_no)
+);	
+
+
+CREATE TABLE  Dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY(emp_no, dept_no)
+);
+CREATE TABLE titles(
+	emp_no VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+
 );
 
-select * from Titles;
+CREATE TABLE dept_manager(
+dept_no VARCHAR (4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE Employees (
+	emp_no INT NOT NULL,	
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT 	NULL,
+FOREIGN KEY(emp_no) REFERENCES Dept_manager (emp_no),
+FOREIGN KEY(emp_no) REFERENCES Dept_emp (emp_no),
+FOREIGN KEY (emp_no) REFERENCES Titles (emp_no),
+	PRIMARY KEY (emp_no)
+	);
+	
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+
+CREATE TABLE  Dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES Department (dept_no),	
+	PRIMARY KEY(emp_no, dept_no)
+);
+CREATE TABLE titles(
+	emp_no VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES Salaries(emp_no)
+	
+);
+select * from Departments;
